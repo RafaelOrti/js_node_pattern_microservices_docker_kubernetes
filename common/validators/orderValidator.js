0d7846045073996b9eed6b5fb56766394f1762b4
RAFAEL ORIOL ORTI CARRIO <raorcar3@gmail.com>
2023-08-08 12:47:09 +0200

    Add common

---
+++ common/validators/orderValidator.js
@@ -0,0 +1,8 @@
+// Ejemplo de validador de datos para pedidos
+function validateOrder(order) {
+  // Lógica de validación aquí
+  // Verificar que los datos del pedido sean válidos
+  // Si la validación falla, lanzar una excepción con un mensaje de error
+}
+
+module.exports = validateOrder;
